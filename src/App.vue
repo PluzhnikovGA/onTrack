95e5352857cc634339f5eb6c77b8655c9c97dcd5
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-09-30 20:13:39 +0200

    Create navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -15,5 +16,48 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
-  <main></main>
+  <main class="flex flex-col flex-grow">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. In, saepe molestiae incidunt ducimus
+    nisi laboriosam perferendis aliquid, porro ipsa iure cupiditate hic alias velit possimus
+    voluptates quas blanditiis culpa. Unde. Lorem ipsum dolor, sit amet consectetur adipisicing
+    elit. Nulla laborum corporis dolorum omnis natus commodi earum amet facilis ratione quaerat
+    neque, accusantium, cum repudiandae debitis quas consectetur suscipit necessitatibus inventore?
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda doloribus et quam incidunt
+    eum fugiat, maxime dolorum eveniet blanditiis sequi at quasi voluptas consequuntur, vero sit id
+    doloremque vitae quae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore,
+    praesentium, quod totam doloremque cum unde rem quo odit corporis porro odio quis ex suscipit,
+    ad mollitia blanditiis consequuntur amet optio? Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Quasi perspiciatis dicta tempora animi error tempore enim ad aperiam molestias
+    unde sunt quae, neque nisi quis sit quos, nulla soluta asperiores. Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit. Quo aspernatur omnis sequi hic, doloremque atque dolorum maxime
+    repellat voluptatum quaerat architecto voluptas provident et molestiae quae dolore, sed
+    excepturi dignissimos? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta incidunt
+    ex deserunt quod quis quam placeat! Laborum natus est hic labore beatae. Iure, eum vitae?
+    Reprehenderit laudantium sit laborum repellendus! Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Rerum perspiciatis, illo minus aliquam eligendi accusantium ipsum ab possimus
+    aliquid, id velit rem ullam consectetur sed quam quod fugit inventore eius! Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. Provident voluptates optio, odio itaque possimus porro
+    amet eius fugiat omnis, illum voluptas fuga rem minus. Dolor reprehenderit consequatur similique
+    rerum debitis.
+  </main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a href="#timeline" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ClockIcon class="h-6 w-6" /> timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#activities" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ListBulletIcon class="h-6 w-6" /> activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#progress" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ChartBarIcon class="h-6 w-6" /> progress
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
