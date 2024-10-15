a8864fcdd97e5e655ba079a7d701c08025432144
Pluzhnikov Grigorii <pluzhnikovga@gmail.com>
2024-10-15 17:57:17 +0200

    Storage refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { createApp } from 'vue';
 
-import { activities } from '@/utils/activity.utils';
-import { isToday } from '@/utils/time.utils';
-import { timelineItems } from '@/utils/timeline.utils';
-
-import * as storage from '@/storage/storage';
+import { loadState, saveState } from '@/storage/storage';
 
 import App from './App.vue';
 import './assets/main.css';
@@ -14,19 +10,5 @@ loadState();
 document.addEventListener('visibilitychange', (): void => {
   document.visibilityState === 'visible' ? loadState() : saveState();
 });
-
-function saveState(): void {
-  storage.save({
-    timelineItems: timelineItems.value,
-    activities: activities.value,
-  });
-}
-
-function loadState(): void {
-  const state = storage.load();
-
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value;
-  activities.value = state.activities || activities.value;
-}
 
 createApp(App).mount('#app');
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,27 +1,25 @@
-import { today } from '@/utils/time.utils';
-
-import type { TActivity } from '@/types/activity.types';
-import type { TTimelineItem } from '@/types/timeline.types';
+import { activities } from '@/utils/activity.utils';
+import { isToday, today } from '@/utils/time.utils';
+import { timelineItems } from '@/utils/timeline.utils';
 
 import { APP_NAME } from '@/constants/page.constants';
 
-type TData = {
-  timelineItems: TTimelineItem[];
-  activities: TActivity[];
-};
-
-type ResponseData = {
-  timelineItems: TTimelineItem[];
-  activities: TActivity[];
-  date: Date;
-};
-
-export function load(): ResponseData {
-  const state = localStorage.getItem(APP_NAME);
-
-  return state ? JSON.parse(state) : {};
+export function saveState(): void {
+  localStorage.setItem(
+    APP_NAME,
+    JSON.stringify({
+      timelineItems: timelineItems.value,
+      activities: activities.value,
+      date: today(),
+    }),
+  );
 }
 
-export function save(data: TData): void {
-  localStorage.setItem(APP_NAME, JSON.stringify({ ...data, date: today().toDateString() }));
+export function loadState(): void {
+  const serializedState = localStorage.getItem(APP_NAME);
+
+  const state = serializedState ? JSON.parse(serializedState) : {};
+
+  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value;
+  activities.value = state.activities || activities.value;
 }
